5d9e97121f35c6c649a7fe95fd77eb17eb6a5a2f
Jørgen Borgesen <joergen.borgesen@gmail.com>
2013-04-18 09:24:45 +0300

    Directive no longer depends on jquery
    Selector 'input[name]' not supported by jqLite. Using 'input' and manually filtering on attribute name

--- src/form-model.js
+++ src/form-model.js
@@ -6,10 +6,12 @@ angular.module('jorgenfb.directives', []).directive('formModel', function() {
         restrict: 'A',
         require: 'form',
         compile: function(element, attr) {
-            var inputs = element.find('input[name]');
+            var inputs = element.find('input');
             for (var i = 0; i < inputs.length; i++){
                 var input = inputs.eq(i);
-                input.attr('ng-model', attr.formModel + '.' + input.attr('name'));
+                if (input.attr('name')) {
+                    input.attr('ng-model', attr.formModel + '.' + input.attr('name'));
+                }
             }
         }
     };
